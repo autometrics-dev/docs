927cf4f136e69918743dd73befe4b7bd5fb37c1a
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-05-12 15:47:24 +0200

    update theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
-import Logo from "./src/components/Header/Logo"
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Logo from "./src/components/Header/Logo";
 
 const config: DocsThemeConfig = {
-  logo: <Logo />,
+	logo: <Logo />,
 	darkMode: false,
-  project: {
-    link: 'https://github.com/autometrics-dev',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/autometrics-dev/docs',
-  footer: {
-    text: 'Autometrics',
-  },
-}
+	project: {
+		link: "https://github.com/autometrics-dev",
+	},
+	chat: {
+		link: "https://discord.com",
+	},
+	docsRepositoryBase: "https://github.com/autometrics-dev/docs",
+	//footer: {
+	//  text: 'Autometrics',
+	//},
+};
 
-export default config
+export default config;
